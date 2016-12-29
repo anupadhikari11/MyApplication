58bc463709338c35d46bf8cb7e3bb08b23ec8a72
Anup <anup@revesoft.com>
2016-12-29 17:23:35 +0530

    3nd commit

--- mylibrary/src/main/java/adhikari/anup/mylibrary/LibActivity.java
+++ mylibrary/src/main/java/adhikari/anup/mylibrary/LibActivity.java
@@ -1,7 +1,10 @@
 package adhikari.anup.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import io.codetail.animation.SupportAnimator;
 
 public class LibActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LibActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lib);
+        LinearLayout mRevealView = (LinearLayout)findViewById(R.id.reveal_items);
+        int cx = (mRevealView.getLeft() + mRevealView.getRight());
+        int cy = mRevealView.getTop();
+        int endradius = Math.max(mRevealView.getWidth(), mRevealView.getHeight());
+        SupportAnimator mAnimator = io.codetail.animation.ViewAnimationUtils.createCircularReveal(mRevealView, cx, cy, 0, endradius);
     }
 }
